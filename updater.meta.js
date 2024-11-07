f1cf2ac4b260f03e967292b7670b54b53a12b264
ImInsane1337 <135440126+ImInsane1337@users.noreply.github.com>
2024-11-07 16:04:45 +0400

    Create updater.meta.js

---
+++ updater.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         BladeUp Customizer
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Изменяет секцию VIP, применяет изменения на кнопках и ролях только если активирована кастомная тема "Customed by ImInsane"
+// @match        *://bladeup.pw/*
+// @grant        none
+// @updateURL    https://example.com/your_script.meta.js
+// @downloadURL  https://example.com/your_script.user.js
+// ==/UserScript==
