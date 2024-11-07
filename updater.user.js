ffb85f44a88663f6300908673e395dc3def2baa0
ImInsane1337 <135440126+ImInsane1337@users.noreply.github.com>
2024-11-07 16:05:55 +0400

    Update updater.user.js

--- updater.user.js
+++ updater.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BladeUp Customizer
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Изменяет секцию VIP, применяет изменения на кнопках и ролях только если активирована кастомная тема "Customed by ImInsane"
 // @match        *://bladeup.pw/*
 // @grant        none
